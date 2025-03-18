14a2ee1c1975a79612dde2a0ccfc9269d41e94b7
Michele Locati <michele@locati.it>
2025-03-18 11:48:18 +0100

    Ignore SASS warnings of dependencies

--- gh-pages/vite.config.ts
+++ gh-pages/vite.config.ts
@@ -4,4 +4,11 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        quietDeps: true,
+      },
+    },
+  },
 })
